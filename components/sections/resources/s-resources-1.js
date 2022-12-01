3d606bd9197ba5b65b000da3cf1337eb556e4e6b
Johannes Weniger <jo@MacBook-Pro-2.Home>
2022-12-01 00:42:46 +0000

    small layout fixed

--- components/sections/resources/s-resources-1.js
+++ components/sections/resources/s-resources-1.js
@@ -3,7 +3,7 @@ function ResourcesSectionOne() {
     <section className="lg:py-40 pt-20">
       <h2 className="lg:text-4xl text-2xl pb-10 lg:ml-4 ml-2 ">Resources</h2>
       <div className="flex items-center lg:p-4 flex-col p-2 lg:py-[5rem] ">
-        <div className="lg:h-[40rem] h-[60rem] lg:w-[70rem] flex  items-end lg:flex-row flex-col shrink bg-green-200">
+        <div className="lg:h-[40rem] h-[60rem] lg:w-[70rem] flex  items-end lg:flex-row flex-col shrink ">
           <div className="lg:h-full lg:w-1/2 w-2/2">
             {" "}
             <div className="lg:h-5/6 h-[15rem] lg:w-full w-4/6 text-white ">
